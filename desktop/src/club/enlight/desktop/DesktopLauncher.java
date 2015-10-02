dca47ebfee34b9ac8212c5a225b4502454b7dfad
Kenneth Siu <kennethksiu@gmail.com>
2015-10-02 17:21:31 -0500

    Initial Files

---
+++ desktop/src/club/enlight/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package club.enlight.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import club.enlight.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
